020ce08d38e899b544b25a4b1c96a5d395972a56
Lujan G <jaralujan0@gmail.com>
2025-08-11 00:11:26 -0300

    Mejoras de funcionalidad y exportacion

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-(function () {
-        function ataqueEspecial(target, propertyKey, descriptor){
-            let contador = 0;
-            const fnOriginal = descriptor.value;
+const { Hechicera, Guardian, Troll, Brujo } = (function () {
+    function ataqueEspecial(target, fn) {
+        let contador = 0;
 
-            descriptor.value = function (...args){
-                contador++;
-                console.log(`${this.constructor.name} batalla numero : ${contador}`);
-                return fnOriginal.call(this, contador);
-            };
+        return function (...args) {
+            contador++;
+            console.log(`${this.constructor.name} batalla numero : ${contador}`);
+            return fn.call(this, contador);
+        };
+    }
 
-            return descriptor;
+    function atacarDecorador(target, fn) {
+        return function (...args) {
+            const enemigo = args[0];
+            console.log(`${this.constructor.name} ha atacado a ${enemigo.constructor.name}`);
+            return fn.apply(this, args);
         }
+    }
 
-    class personajes {
+    class Personajes {
         constructor(puntosAtaque, escudo) {
             this.life = 1500;
             this.puntosAtaque = puntosAtaque;
@@ -21,73 +26,79 @@
 
         set setlife(posAtaque) {
             this.life = posAtaque;
-            if(this.life <= 0){
+            if (this.life <= 0) {
                 console.log("El personaje a sido derrotado");
             }
         }
 
-        recibirDaño(ataqueEnemigo){
-            let dañoReal = ataqueEnemigo - escudo;
+        recibirDaño(ataqueEnemigo) {
+            let dañoReal = ataqueEnemigo - this.escudo;
             let daño = (dañoReal > 0) ? this.life - dañoReal : this.life;
             this.setlife = daño;
         }
+
+        atacar(enemigo) {
+            enemigo.recibirDaño(this.puntosAtaque);
+        }
     }
 
-    class hechicera extends personajes {
+    Personajes.prototype.atacar = atacarDecorador(Personajes.prototype, Personajes.prototype.atacar);
+
+    class Hechicera extends Personajes {
         constructor(puntosAtaque = 350, escudo = 175) {
             super(puntosAtaque, escudo)
             this.descripcion = `Hechicera: esta hermosa joven del bosque cuenta con un poder de ataque de ${this.puntosAtaque} y es capaz de generar un campo de proteccion de ${this.escudo} puntos de fuerza. Su escudo la mantiene protegida durante toda la batalla y cada tres ataques realizados se regenera 150 puntos de vida.`
         }
 
-        @ataqueEspecial
-        atacar(nBatalla){
-            if(nBatalla % 3 === 0){
+        activarHabilidad(nBatalla) {
+            if (nBatalla % 3 === 0) {
                 this.setlife = this.life + 150;
             }
         }
     }
 
-    class guardian extends personajes {
+    class Guardian extends Personajes {
         constructor(puntosAtaque = 450, escudo = 200) {
             super(puntosAtaque, escudo)
             this.descripcion = `Guardian: fuerte, valiente y perseverante aunque no muy resistente. Cuenta con un ataque de ${this.puntosAtaque} puntos pero a medida que avanza la batalla va perdiendo 15 puntos de fuerza.`
         }
 
-        @ataqueEspecial
-        atacar(){
+        activarHabilidad(nBatalla) {
             this.puntosAtaque = Math.max(0, this.puntosAtaque - 15);
         }
     }
 
-    class troll extends personajes {
+    class Troll extends Personajes {
         constructor(puntosAtaque = 200, escudo = 400) {
             super(puntosAtaque, escudo)
             this.descripcion = `Troll: su cuerpo rocoso le brinda una proteccion de ${this.escudo} puntos y es capaz de resistir 5 batallas casi sin ningun rasguño. A pesar de su apariencia ruda su ataque es de ${this.puntosAtaque}.`
         }
 
-        @ataqueEspecial
-        atacar(nBatalla){
-            if(nBatalla % 5 === 0){
+        activarHabilidad(nBatalla) {
+            if (nBatalla % 5 === 0) {
                 this.escudo = 0;
             }
         }
     }
 
-    class brujo extends personajes {
+    class Brujo extends Personajes {
         constructor(puntosAtaque = 250, escudo = 200) {
             super(puntosAtaque, escudo)
             this.descripcion = `Brujo: tantos años en el bosque lo debilitaron bastante pero le dieron la experiencia necesaria para conjurar hechizos de fuerza. Cuenta con un poder de ataque de ${this.puntosAtaque} puntos y cada tres batallas lanza un hechizo el triple de fuerte, su conjuro de proteccion es de ${this.escudo} puntos.`
         }
 
-        @ataqueEspecial
-        atacar(nBatalla){
+        activarHabilidad(nBatalla) {
             let punAtaque = this.puntosAtaque;
-            if(nBatalla % 3 === 0){
+            if (nBatalla % 3 === 0) {
                 this.puntosAtaque *= 3;
             }
-            this.puntosAtaque =  punAtaque;
         }
     }
 
-    return {}
+    Hechicera.prototype.activarHabilidad = ataqueEspecial(Hechicera.prototype, Hechicera.prototype.activarHabilidad)
+    Guardian.prototype.activarHabilidad = ataqueEspecial(Guardian.prototype, Guardian.prototype.activarHabilidad)
+    Troll.prototype.activarHabilidad = ataqueEspecial(Troll.prototype, Troll.prototype.activarHabilidad)
+    Brujo.prototype.activarHabilidad = ataqueEspecial(Brujo.prototype, Brujo.prototype.activarHabilidad)
+
+    return { Hechicera, Guardian, Troll, Brujo }
 })();
