62444a06b0ebb9c34cbe5b4e90246be97407bdad
Lujan G <jaralujan0@gmail.com>
2025-08-13 23:26:31 -0300

    Correccion y mejoras del modulo de batalla, correcion de variables en modulo personajes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const { Hechicera, Guardian, Troll, Brujo } = (function () {
+const Personajes = (function () {
     function ataqueEspecial(target, fn) {
         let contador = 0;
 
         return function (...args) {
             contador++;
-            console.log(`${this.constructor.name} batalla numero : ${contador}`);
+            console.log('************************');
+            console.log(`Batalla numero : ${contador}`);
             return fn.call(this, contador);
         };
     }
@@ -18,23 +19,33 @@ const { Hechicera, Guardian, Troll, Brujo } = (function () {
     }
 
     class Personajes {
+        #life = 1500;
+        #puntosAtaque;
+        #escudo;
         constructor(puntosAtaque, escudo) {
-            this.life = 1500;
-            this.puntosAtaque = puntosAtaque;
-            this.escudo = escudo
+            this.#puntosAtaque = puntosAtaque;
+            this.#escudo = escudo
         }
 
-        set setlife(posAtaque) {
-            this.life = posAtaque;
-            if (this.life <= 0) {
+        get life() { return this.#life };
+        set life(posAtaque) {
+            this.#life = posAtaque;
+            if (this.#life <= 0) {
                 console.log("El personaje a sido derrotado");
             }
         }
 
+        get escudo() { return this.#escudo };
+        set escudo(nuevoEscudo) { this.#escudo = nuevoEscudo };
+
+        get puntosAtaque() { return this.#puntosAtaque };
+        set puntosAtaque(nuevoAtaque) { this.#puntosAtaque = nuevoAtaque };
+
         recibirDaño(ataqueEnemigo) {
             let dañoReal = ataqueEnemigo - this.escudo;
             let daño = (dañoReal > 0) ? this.life - dañoReal : this.life;
-            this.setlife = daño;
+            this.life = daño;
+            console.log(`${this.constructor.name} ha recibido ${dañoReal} puntos de daño, vida restante: ${this.life}`);
         }
 
         atacar(enemigo) {
@@ -52,7 +63,8 @@ const { Hechicera, Guardian, Troll, Brujo } = (function () {
 
         activarHabilidad(nBatalla) {
             if (nBatalla % 3 === 0) {
-                this.setlife = this.life + 150;
+                this.life = this.life + 150;
+                console.log(`${this.constructor.name} ha regenerado 150 puntos de vida, su vida actual es de ${this.life}`);
             }
         }
     }
@@ -65,6 +77,7 @@ const { Hechicera, Guardian, Troll, Brujo } = (function () {
 
         activarHabilidad(nBatalla) {
             this.puntosAtaque = Math.max(0, this.puntosAtaque - 15);
+            console.log(`${this.constructor.name} ha perdido 15 puntos de ataque, su ataque actual es de ${this.puntosAtaque}`);
         }
     }
 
@@ -77,6 +90,7 @@ const { Hechicera, Guardian, Troll, Brujo } = (function () {
         activarHabilidad(nBatalla) {
             if (nBatalla % 5 === 0) {
                 this.escudo = 0;
+                console.log(`${this.constructor.name} ha perdido su escudo`);
             }
         }
     }
@@ -91,6 +105,7 @@ const { Hechicera, Guardian, Troll, Brujo } = (function () {
             let punAtaque = this.puntosAtaque;
             if (nBatalla % 3 === 0) {
                 this.puntosAtaque *= 3;
+                console.log(`${this.constructor.name} ha activado su hechizo, su ataque es de ${this.puntosAtaque}`);
             }
         }
     }
@@ -101,4 +116,44 @@ const { Hechicera, Guardian, Troll, Brujo } = (function () {
     Brujo.prototype.activarHabilidad = ataqueEspecial(Brujo.prototype, Brujo.prototype.activarHabilidad)
 
     return { Hechicera, Guardian, Troll, Brujo }
+})();
+
+
+const batalla = (function () {
+    const personajesValidos = Personajes;
+
+    let personaje1 = null;
+    let personaje2 = null;
+
+    do {
+        personaje1 = prompt("Escribe el nombre del personaje para jugar: Hechicera, Guardian, Troll, Brujo")
+        personaje2 = prompt("Escribe el nombre del personaje para jugar: Hechicera, Guardian, Troll, Brujo")
+    } while (personaje1 === null || personaje2 === null)
+
+    const jugador1 = new personajesValidos[personaje1]();
+    const jugador2 = new personajesValidos[personaje2]();
+
+    confirm('Bienvenido a la batalla, es hora de comenzar!!')
+    let dado = Math.floor(Math.random() * 10);
+    let j1, j2;
+   
+    if (dado % 2 == 0) {
+        j1 = jugador1;
+        j2 = jugador2;
+        console.log(`${j1.constructor.name} ataca primero`)
+    } else {
+        j1 = jugador2;
+        j2 = jugador1;
+        console.log(`${j1.constructor.name} ataca primero`)
+    }
+    console.log('************************')
+
+    while (j1.life > 0 && j2.life > 0) {
+        j1.activarHabilidad();
+        j1.atacar(j2);
+        if (j2.life <= 0) break;
+        j2.activarHabilidad();
+        j2.atacar(j1);
+    }
+    confirm('La batalla ha terminado');
 })();
